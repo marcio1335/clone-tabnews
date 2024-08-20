de1c8e0ff0c95ecad578433fbc4c42955620289f
Marcio Andre <143571799+marcio1335@users.noreply.github.com>
2024-08-20 20:24:40 +0000

    fiz besteira no forçando o commit passado kk

--- pages/index.js
+++ pages/index.js
@@ -19,6 +19,7 @@ export default function Profile() {
                         }}
                     />
                 </Link>
+            <h3>Olá, eu tenho 22 anos e estou tentando me tornar um programador!</h3>
         </>
     );
 }
